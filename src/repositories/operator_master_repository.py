69762895e105c0bde3bfa0ba2b907b0284369489
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-05-07 17:03:43 +0900

    refactor

--- src/repositories/operator_master_repository.py
+++ src/repositories/operator_master_repository.py
@@ -8,27 +8,6 @@ from src.repositories.repository import Repository
 class OperatorMasterRepository(Repository):
     TABLE_NAME = TableNames.OPERATOR_MASTER
 
-    @classmethod
-    def find_main_stat_by_operator_id(
-        cls,
-        operator_id: str,
-    ) -> int | None:
-        with sqlite3.connect(DB_PATH) as conn:
-            conn.row_factory = sqlite3.Row
-
-            row = conn.execute(
-                f"""
-                SELECT base_atk
-                FROM {cls.TABLE_NAME}
-                WHERE operator_id = ?
-                """,
-                (operator_id),
-            ).fetchone()
-
-            if row is None:
-                return None
-            
-            return row["base_atk"]
         
     @classmethod
     def find_by_operator_id_and_column(
