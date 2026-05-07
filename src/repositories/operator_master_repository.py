b052611e4e4d40d021dc93cf38e171a61331e0a2
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-05-07 18:57:10 +0900

    refactor

--- src/repositories/operator_master_repository.py
+++ src/repositories/operator_master_repository.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+from src.entities.operator_condition import OperatorCondition
 from src.constants.paths import DB_PATH
 from src.constants.database_table_names import TableNames
 from src.repositories.repository import Repository
@@ -12,22 +13,22 @@ class OperatorMasterRepository(Repository):
     @classmethod
     def find_by_operator_id_and_column(
         cls,
-        operator_id: str,
-        column: str,
+        cond: OperatorCondition,
+        attr: str,
     ) -> int | None:
         with sqlite3.connect(DB_PATH) as conn:
             conn.row_factory = sqlite3.Row
 
             row = conn.execute(
                 f"""
-                SELECT {column}
+                SELECT {attr}
                 FROM {cls.TABLE_NAME}
                 WHERE operator_id = ?
                 """,
-                (operator_id,),
+                (cond.operator_id,),
             ).fetchone()
 
             if row is None:
                 return None
             
-            return row[column]
+            return row[attr]
--- tests/test_operator_master_repository.py
+++ tests/test_operator_master_repository.py
@@ -1,14 +1,15 @@
+from src.entities.operator_condition import OperatorCondition
 from src.repositories.operator_master_repository import OperatorMasterRepository
 from src.constants.database_columns import OperatorMasterColumns
 
 
 def test_find_by_operator_id_and_column():
-    operator_id = "lifeng"
-    column = OperatorMasterColumns.MAIN_STAT
+    cond = OperatorCondition(operator_id="lifeng", level=1)
+    attr = OperatorMasterColumns.MAIN_STAT
 
     main_stat = OperatorMasterRepository.find_by_operator_id_and_column(
-        operator_id,
-        column,
+        cond,
+        attr,
     )
     expected = "agility"
 
