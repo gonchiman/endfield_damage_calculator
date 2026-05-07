61fa00477c20c774ffd92d411e2c10b940dee770
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-05-07 17:04:02 +0900

    update

--- src/constants/database_columns.py
+++ src/constants/database_columns.py
@@ -3,3 +3,13 @@ class OperatorMasterColumns:
     OPERATOR_NAME = "operaotr_name"
     MAIN_STAT = "main_stat"
     SUB_STAT = "sub_stat"
+
+
+class OperatorStatusColumns:
+    OPERATOR_ID = "operator_id"
+    LEVEL = "level"
+    STRENGTH = "strength"
+    AGILITY = "agility"
+    INTELLECT = "intellect"
+    WILL = "will"
+    BASE_ATK = "base_atk"
